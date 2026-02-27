74b6f66aacef268feba3e32991365ef73f23664a
dope <jaamaaj26@gmail.com>
2026-02-27 14:46:29 +0800

    testt

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,7 @@
-import next from "next";
+import Header from "@/Header/page";
 
 export default function Home() {
   return (
-    <div className="">
-      <main className="">
-        <a className="cursor-pointer p-2">logo</a>
-        <a className="cursor-pointer p-2">Нүүр</a>
-        <a className="cursor-pointer p-2">Бүтээгдэхүүн</a>
-      </main>
-    </div>
+    <Header/>
   );
 }
