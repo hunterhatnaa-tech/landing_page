852abf65c0d5eb510231ef262bfffc3995bd8f5e
KrowKuro <kkrowkkuro@gmail.com>
2026-02-27 16:26:25 +0800

    early routing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,8 @@
-<<<<<<< HEAD
 import next from "next";
 import Link from "next/link";
-=======
-import Header from "@/Header/page";
->>>>>>> 74b6f66aacef268feba3e32991365ef73f23664a
 
 export default function Home() {
   return (
-    <Header/>
+      <h1>THIS IS HOME</h1>
   );
 }
---
+++ src/app/products/page.tsx
@@ -0,0 +1,5 @@
+export default function Products() {
+    return(
+        <h1>PRODUCTS WILL BE HERE!</h1>
+    );
+}
